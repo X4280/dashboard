790cb46774f9389a9512f213f7988f9ea8c3352d
X4280 <65620483+X4280@users.noreply.github.com>
2025-07-27 19:20:39 +0200

    Delete _app.js

--- _app.js
+++
@@ -1,7 +0,0 @@
-import 'react-grid-layout/css/styles.css';
-import 'react-resizable/css/styles.css';
-import '../styles/globals.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
