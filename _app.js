496673e124548fa654c4536b10f44207f34f2518
X4280 <65620483+X4280@users.noreply.github.com>
2025-07-27 15:52:10 +0200

    Create _app.js

---
+++ _app.js
@@ -0,0 +1,7 @@
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
